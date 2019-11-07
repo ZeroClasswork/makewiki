f3c6b4b5aa135a21e9f60b7de6ac6f8a653cdf83
atleastzero <megan.obryan52@gmail.com>
2019-11-07 14:44:09 -0800

    some stretch challenges

--- makewiki/urls.py
+++ makewiki/urls.py
@@ -16,12 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-"""
-CHALLENGES:
-    1. Uncomment the path() for the wiki app below. Use it to direct any request (except `/admin` URLs)
-        to the the `wiki` app's URL configuration. Use the above docstring to guide you if you feel stuck.
-    2. Make sure Django doesn't give you any warnings or errors when you execute `python manage.py runserver`.
-"""
 urlpatterns = [
     # Admin Site
     path('admin/', admin.site.urls),
